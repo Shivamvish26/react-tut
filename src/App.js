55b1fbd30dfb42a212630a6b649dbd6bb18bdd22
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-10-22 16:56:56 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Userclas from "./Components/Userclas/UserClass";
 import Click from "./Components/Event/Click";
 import Hookusestate from "./Components/Hooks/Hookusestate";
 import Clicksecond from "./Components/Hooks/Clicksecond";
-import Propsfun from "./Components/Propsfun/Propfun";
+import Propsfun from "./Components/Propsfun&class/Propfun";
+import Propsclass from "./Components/Propsfun&class/Propsclass";
 
 function App() {
   // function in function
@@ -13,7 +14,7 @@ function App() {
     return <div>Apple Component</div>;
   }
 
-  const [name, setName] = useState("Shubham")
+  const [name, setName] = useState("Shubham");
 
   return (
     <div className="App">
@@ -23,9 +24,14 @@ function App() {
       {/* <Click/> there are n number of evennt can be used in JSX just go through out the React js offical website */}
       {/* <Hookusestate/> state with function component */}
       {/* <Clicksecond/> */}
-      {/* The following names are been changes using states using onclick event*/}
-      <Propsfun name={name}/>
-      <button onClick={()=>setName("Shivam")}>Click Me</button>
+
+      {/* The following names are been changes using states using onclick event* {Function in props}/}
+      {/* <Propsfun name={name}/>
+      <button onClick={()=>setName("Shivam")}>Click Me</button> */}
+      {/*  */}
+
+      {/*       The following names are been changes using states using onclick event* {Class in props}*/}
+      <Propsclass name="Shubham" />
       {/*  */}
     </div>
   );
