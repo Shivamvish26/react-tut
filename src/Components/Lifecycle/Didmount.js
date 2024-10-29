680b9570774445fa0a61d55c87b18076ed3924a3
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-10-29 11:08:44 +0530

    file added

--- src/Components/Lifecycle/Didmount.js
+++ src/Components/Lifecycle/Didmount.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 
+// export default class Didmount extends Component {
+//   constructor() {
+//     super();
+//     this.state={
+//       name:"name"
+//     }
+//     console.log("Constructor");
+//   }
+
+//   componentDidMount() {
+//     console.log("ComponentDidMount");
+//   }
+
+//   render() {
+//     console.log("Render");
+//     return (
+//       <div>
+//         <h1>Component Did Mount Lifecycle {this.state.name}</h1>
+//         <button onClick={()=>{this.setState({name:"Shivam"})}}>Update Name</button>
+//       </div>
+//     );
+//   }
+// }
+
 export default class Didmount extends Component {
   constructor() {
     super();
-    this.state={
-      name:"name"
-    }
-    console.log("Constructor");
+    this.state = {
+      name: "",
+    };
+    // console.log("Constructor");
   }
 
   componentDidMount() {
-    console.log("ComponentDidMount");
+    this.setState({ name: "Shivam" });
   }
 
   render() {
@@ -18,7 +42,13 @@ export default class Didmount extends Component {
     return (
       <div>
         <h1>Component Did Mount Lifecycle {this.state.name}</h1>
-        <button onClick={()=>{this.setState({name:"Shivam"})}}>Update Name</button>
+        <button
+          onClick={() => {
+            this.setState({ name: "Shubham" });
+          }}
+        >
+          Update Name
+        </button>
       </div>
     );
   }
