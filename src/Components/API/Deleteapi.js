701cccd98f61b264abb4c9f25c1f22c66ed5c355
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-11-18 16:10:52 +0530

    file changes

--- src/Components/API/Deleteapi.js
+++ src/Components/API/Deleteapi.js
@@ -1,13 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Table, Button } from "react-bootstrap";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Deleteapi() {
   const [data, setData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("darkMode");
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [id, setId] = useState("");
 
   const toggleTheme = () => {
     setDarkMode((prevMode) => !prevMode);
   };
+
+  function getlist() {
+    fetch("https://jsonplaceholder.typicode.com/comments?postId=1").then(
+      (result) => {
+        result.json().then((resp) => {
+          setData(resp);
+          setName(resp[0].name);
+          setEmail(resp[0].email);
+          setId(resp[0].id);
+        });
+      }
+    );
+  }
+
+  function updateuser(id) {
+    console.log(data[id - 1]);
+    setName(data[id - 1].name);
+    setEmail(data[id - 1].email);
+    setId(data[id - 1].id);
+  }
 
   function handleuserdelete(id) {
     fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
@@ -20,14 +48,22 @@ export default function Deleteapi() {
     });
   }
 
-  function getlist() {
-    fetch("https://jsonplaceholder.typicode.com/comments?postId=1").then(
-      (result) => {
-        result.json().then((resp) => {
-          setData(resp);
-        });
-      }
-    );
+  function updateuserslist() {
+    // console.warn(name, email, id);
+    let item = { name, email, id };
+    fetch(`https://jsonplaceholder.typicode.com/comments/${id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    }).then((result) => {
+      result.json().then((resp) => {
+        console.log(resp);
+        getlist();
+      });
+    });
   }
 
   useEffect(() => {
@@ -88,16 +124,55 @@ export default function Deleteapi() {
                 <td>{item.email}</td>
                 <td>
                   <Button
-                    className="btn btn-danger text-white"
+                    className="btn btn-danger text-white mr-2"
                     onClick={() => handleuserdelete(item.id)}
                   >
                     Delete
+                  </Button>
+                  <Button
+                    className="btn btn-danger text-white"
+                    onClick={() => updateuser(item.id)}
+                  >
+                    Update
                   </Button>
                 </td>
               </tr>
             ))}
           </tbody>
         </Table>
+      </div>
+      {/* Prefilled Values */}
+      <div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            placeholder="Enter name"
+            className="p-2"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />{" "}
+          <br />
+          <input
+            placeholder="Enter email"
+            className="p-2 mt-2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />{" "}
+          <br />
+          <Button className="btn btn-primary mt-2" onClick={updateuserslist}>
+            Update User
+          </Button>
+        </form>
+        <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        progress={undefined}
+        theme="light"
+        transition={Bounce}
+      />
       </div>
     </div>
   );
